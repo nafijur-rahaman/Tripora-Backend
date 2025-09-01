9c6dbb1f0343be4f10d958c255dd81eae8e92f4a
nafijur-rahaman <20234103116@cse.bubt.edu.bd>
2025-09-02 01:58:28 +0600

    init project and and setup mongodb

---
+++ index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// MongoDB connection URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1e3hmt0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db(process.env.DB_NAME);
+    const packagesCollection = db.collection("packages");
+    const categoriesCollection = db.collection("categories");
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("✅ Successfully connected and pinged MongoDB!");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error);
+  } }
+
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
